317eb3535104017f753d91ee285be939756f08d0
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2026-04-22 11:05:56 +0500

    Обновил контент главной страницы

--- app.py
+++ app.py
@@ -2,6 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
- return '<h1>Привет из Railway!</h1>'
-if __name__ == '__main__':
- app.run(host='0.0.0.0', port=8080)
+ return '<h1>Railway Deployed!</h1><p>Auto-deployed from GitHub!</p>'
