5f5548963e8290d5dd31028ba27b982db4f590ae
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2026-04-22 11:11:05 +0500

    fix

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
- return '<h1>Railway Deployed!</h1><p>Auto-deployed from GitHub!</p>'
+	return'<h1>Railway Deployed!</h1><p>Auto-deployed from GitHub!</p>'
