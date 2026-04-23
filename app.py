a6e6572142e3574fe883d64a882104d3b30b15ef
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2026-04-23 08:53:50 +0500

    fa

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
- return '<h1>Привет из Railway!</h1>'
+ return '<h1>Railway Deployed!</h1><p>Auto-deployed from GitHub!</p>'
 
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=8080)
